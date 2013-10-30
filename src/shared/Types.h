2dc6263a13d05b9ca17266d087ab090304b93f41
monocasa <trismandev@gmail.com>
2013-10-29 20:09:01 -0600

    Rearrange XenonPpcContext to put quickly accessible gprs together

--- src/shared/Types.h
+++ src/shared/Types.h
@@ -29,21 +29,22 @@ struct XenonPpcContext : public CpuContext {
 	};
 
 	uint64_t gpr[32];
-	uint64_t pc;
 	uint64_t lr;
 	uint64_t ctr;
+	uint64_t sprg0;
+	uint64_t sprg1;
+	uint64_t sprg2;
+	uint64_t sprg3;
 
 	uint64_t fpr[32];
 	VmxReg   vmx[128];
+
+	uint64_t pc;
 
 	uint64_t msr;
 	uint64_t hrmor;
 	uint64_t srr0;
 	uint64_t srr1;
-	uint64_t sprg0;
-	uint64_t sprg1;
-	uint64_t sprg2;
-	uint64_t sprg3;
 	uint64_t hid6;
 	uint64_t lpidr;
 	uint64_t lpcr;
