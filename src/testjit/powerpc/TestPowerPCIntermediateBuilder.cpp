d8e7ae8d85a59662f3adbae6f0e4c890e78f3ceb
monocasa <trismandev@gmail.com>
2013-11-03 19:07:11 -0700

    Add sign extension tests to addi and addis

--- src/testjit/powerpc/TestPowerPCIntermediateBuilder.cpp
+++ src/testjit/powerpc/TestPowerPCIntermediateBuilder.cpp
@@ -15,6 +15,11 @@ TEST(PowerPCIntermediateBuilder, Addi)
 	EXPECT_EQ( 3 * sizeof(uint64_t), instr[0].args[0] );
 	EXPECT_EQ( 0x00000001, instr[0].args[1] );
 
+	EXPECT_EQ( 1, builder.BuildIntermediate( instr, 0x3860FFFF, 0 ) );
+	EXPECT_EQ( InstrOp::LOAD_IMM, instr[0].op );
+	EXPECT_EQ( 3 * sizeof(uint64_t), instr[0].args[0] );
+	EXPECT_EQ( 0xFFFFFFFFFFFFFFFFUL, instr[0].args[1] );
+
 	EXPECT_EQ( 1, builder.BuildIntermediate( instr, 0x38631000, 0 ) );
 	EXPECT_EQ( InstrOp::ADD_IMM, instr[0].op );
 	EXPECT_EQ( 3 * sizeof(uint64_t), instr[0].args[0] );
@@ -31,6 +36,11 @@ TEST(PowerPCIntermediateBuilder, Addis)
 	EXPECT_EQ( InstrOp::LOAD_IMM, instr[0].op );
 	EXPECT_EQ( 3 * sizeof(uint64_t), instr[0].args[0] );
 	EXPECT_EQ( 0x00010000, instr[0].args[1] );
+
+	EXPECT_EQ( 1, builder.BuildIntermediate( instr, 0x3C60FFFF, 0 ) );
+	EXPECT_EQ( InstrOp::LOAD_IMM, instr[0].op );
+	EXPECT_EQ( 3 * sizeof(uint64_t), instr[0].args[0] );
+	EXPECT_EQ( 0xFFFFFFFFFFFF0000UL, instr[0].args[1] );
 
 	EXPECT_EQ( 1, builder.BuildIntermediate( instr, 0x3c631000, 0 ) );
 	EXPECT_EQ( InstrOp::ADD_IMM, instr[0].op );
